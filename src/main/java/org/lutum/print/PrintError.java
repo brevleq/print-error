aae7234ca62443e83428dfd19806a852b857da45
Hudson O. Assumpção <brevleq@gmail.com>
2014-11-11 10:51:49 -0200

    Creation of project

---
+++ src/main/java/org/lutum/print/PrintError.java
@@ -0,0 +1,49 @@
+package org.lutum.print;
+
+import javafx.application.Application;
+import javafx.print.PrinterJob;
+import javafx.stage.Stage;
+
+import javax.print.*;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.JobName;
+import java.util.Locale;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hudson
+ * Date: 11/11/14
+ * Time: 10:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrintError extends Application {
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws PrintException {
+        PrinterJob printerJob = PrinterJob.createPrinterJob();
+        printerJob.showPrintDialog(stage);
+
+
+        PrintRequestAttributeSet printRequestAttributeSet = new HashPrintRequestAttributeSet();
+        printRequestAttributeSet.add(new Copies(printerJob.getJobSettings().getCopies()));
+        printRequestAttributeSet.add(new JobName("test", Locale.getDefault()));
+        DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
+        Doc mydoc = new SimpleDoc(ClassLoader.class.getResourceAsStream("/should-be-printed.txt"), flavor, null);
+        DocPrintJob job = recuperarImpressora(printerJob.getPrinter().getName()).createPrintJob();
+        job.print(mydoc, printRequestAttributeSet);
+    }
+
+    private PrintService recuperarImpressora(String name) {
+        for (PrintService printService : java.awt.print.PrinterJob.lookupPrintServices()) {
+            if (name.equalsIgnoreCase(printService.getName())) {
+                return printService;
+            }
+        }
+        return null;
+    }
+}
